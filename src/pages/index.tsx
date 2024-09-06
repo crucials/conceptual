dea96960d0f5fb25fd5d388cbe8f50ae2ec4fe07
crucials <ensueness@gmail.com>
2024-09-06 09:14:43 +0500

    remove antd code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import { Button } from 'antd/lib'
-
 export default function Home() {
     return (
         <>
-            <Button onClick={() => window.alert('gt')} color='blue'>
+            <button onClick={() => window.alert('gt')} color='blue'>
                 Close
-            </Button>
+            </button>
         </>
     )
 }
