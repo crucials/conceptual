f6da090270893f9d28967de72197a634c8290aab
crucials <ensueness@gmail.com>
2024-09-09 09:35:50 +0500

    update app metadata

--- src/pages/ideas/[id].tsx
+++ src/pages/ideas/[id].tsx
@@ -13,6 +13,7 @@ import { localIdeaDeletionThunk, localIdeaSynchronizationThunk } from '@/stores/
 import type { Idea } from '@/types/idea'
 import IdeaPreview from '@/components/idea-page/preview'
 import IdeaEditor from '@/components/idea-page/editor'
+import Head from 'next/head'
 
 export default function Idea() {
     const router = useRouter()
@@ -69,55 +70,61 @@ export default function Idea() {
     }
 
     return (
-        <Container fluid p="xl">
-            <Transition
-                mounted={localIdeas.status === 'loaded' && idea !== undefined}
-                transition="fade"
-            >
-                {transitionStyles => (
-                    <div style={transitionStyles}>
-                        <Tabs defaultValue="preview" variant="outline">
-                            <Tabs.List mb="lg">
-                                <Tabs.Tab
-                                    value="preview"
-                                    leftSection={<IconEye size={22} />}
-                                >
-                                    preview
-                                </Tabs.Tab>
+        <>
+            <Head>
+                <title>{idea?.title || 'idea'}</title>
+            </Head>
 
-                                <Tabs.Tab
-                                    value="edit"
-                                    leftSection={<IconPencil size={22} />}
-                                >
-                                    edit
-                                </Tabs.Tab>
-                            </Tabs.List>
+            <Container fluid p="xl">
+                <Transition
+                    mounted={localIdeas.status === 'loaded' && idea !== undefined}
+                    transition="fade"
+                >
+                    {transitionStyles => (
+                        <div style={transitionStyles}>
+                            <Tabs defaultValue="preview" variant="outline">
+                                <Tabs.List mb="lg">
+                                    <Tabs.Tab
+                                        value="preview"
+                                        leftSection={<IconEye size={22} />}
+                                    >
+                                        preview
+                                    </Tabs.Tab>
 
-                            <Tabs.Panel value="preview">
-                                <IdeaPreview idea={idea} />
-                            </Tabs.Panel>
+                                    <Tabs.Tab
+                                        value="edit"
+                                        leftSection={<IconPencil size={22} />}
+                                    >
+                                        edit
+                                    </Tabs.Tab>
+                                </Tabs.List>
 
-                            <Tabs.Panel value="edit">
-                                <IdeaEditor
-                                    idea={idea}
-                                    onIdeaUpdate={handleIdeaUpdate}
-                                    onIdeaDeletion={handleIdeaDeletion}
-                                    styles={styles}
-                                />
-                            </Tabs.Panel>
-                        </Tabs>
+                                <Tabs.Panel value="preview">
+                                    <IdeaPreview idea={idea} />
+                                </Tabs.Panel>
+
+                                <Tabs.Panel value="edit">
+                                    <IdeaEditor
+                                        idea={idea}
+                                        onIdeaUpdate={handleIdeaUpdate}
+                                        onIdeaDeletion={handleIdeaDeletion}
+                                        styles={styles}
+                                    />
+                                </Tabs.Panel>
+                            </Tabs>
+                        </div>
+                    )}
+                </Transition>
+
+                {localIdeas.status === 'pending' && (
+                    <div>
+                        <Skeleton height={35} mb="lg" />
+
+                        <Skeleton height={12} mb="sm" width="80%" />
+                        <Skeleton height={12} mb="sm" width="70%" />
                     </div>
                 )}
-            </Transition>
-
-            {localIdeas.status === 'pending' && (
-                <div>
-                    <Skeleton height={35} mb="lg" />
-
-                    <Skeleton height={12} mb="sm" width="80%" />
-                    <Skeleton height={12} mb="sm" width="70%" />
-                </div>
-            )}
-        </Container>
+            </Container>
+        </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
+import Head from 'next/head'
 import { Container, Text, Title } from '@mantine/core'
 
 export default function HomePage() {
     return (
-        <Container fluid p="xl">
-            <Title order={1} mb="md">
-                have an idea?
-            </Title>
+        <>
+            <Head>
+                <title>your ideas</title>
+            </Head>
 
-            <Text>select or create a concept in sidebar</Text>
-        </Container>
+            <Container fluid p="xl">
+                <Title order={1} mb="md">
+                    have an idea?
+                </Title>
+
+                <Text>select or create a concept in the sidebar</Text>
+            </Container>
+        </>
     )
 }
